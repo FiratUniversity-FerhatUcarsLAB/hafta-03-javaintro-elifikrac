94b0056ba8b91c84565630308ed051631e41310f
elifikrac <250541010@firat.edu.tr>
2025-10-22 12:07:43 +0300

    Add files via upload

---
+++ Task-2-Matematikselislemler/ToplamaSerisi.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ToplamaSerisi {
+    public static void main(String[] args) {
+        int sonuc = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        System.out.println("1+2+3+4+5+6+7+8+9 =" + sonuc);
+
+    }
+}
