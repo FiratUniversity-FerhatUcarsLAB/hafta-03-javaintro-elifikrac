1763263cf2b85265336f4eb9f8ec37585be44af8
elifikrac <250541010@firat.edu.tr>
2025-10-22 12:37:01 +0300

    Add files via upload

---
+++ Task-5-TabloDesenleri/DonusumTablosu.java
@@ -0,0 +1,23 @@
+public class DonusumTablosu {
+    public static void main(String[] args) {
+
+                System.out.println("Mil\tKilometre");
+                System.out.println("--------------------");
+
+                double mil1 = 1;
+                System.out.println(mil1 + "\t" + (mil1 * 1.609));
+
+                double mil2 = 5;
+                System.out.println(mil2 + "\t" + (mil2 * 1.609));
+
+                double mil3 = 10;
+                System.out.println(mil3 + "\t" + (mil3 * 1.609));
+
+                double mil4 = 20;
+                System.out.println(mil4 + "\t" + (mil4 * 1.609));
+
+                double mil5 = 50;
+                System.out.println(mil5 + "\t" + (mil5 * 1.609));
+            }
+        }
+
---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,27 @@
+public class UsluSayilarTablosu {
+    public static void main(String[] args) {
+
+        System.out.println("a\t a^2\t a^3");
+        System.out.println("-------------------");
+
+        // 1
+        int a1 = 1;
+        System.out.println(a1 + "\t " + (a1 * a1) + "\t " + (a1 * a1 * a1));
+
+        // 2
+        int a2 = 2;
+        System.out.println(a2 + "\t " + (a2 * a2) + "\t " + (a2 * a2 * a2));
+
+        // 3
+        int a3 = 3;
+        System.out.println(a3 + "\t " + (a3 * a3) + "\t " + (a3 * a3 * a3));
+
+        // 4
+        int a4 = 4;
+        System.out.println(a4 + "\t " + (a4 * a4) + "\t " + (a4 * a4 * a4));
+
+        // 5
+        int a5 = 5;
+        System.out.println(a5 + "\t " + (a5 * a5) + "\t " + (a5 * a5 * a5));
+    }
+}
