3823b6d29f0968d7311161814234491e113e1c03
elifikrac <250541010@firat.edu.tr>
2025-10-22 12:31:07 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/İsimKarti.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class İsimKarti {
+    public static void main(String[] args) {
+        String ad = "Elif İkra";
+        String soyad = "Çakmak";
+        String ogrNo = "250541010";
+        String bolum = "Yazilim Muhendisligi";
+
+        System.out.println("=========================");
+        System.out.println("OGRENCI BILGILERI");
+        System.out.println("=========================");
+        System.out.println("Ad :" + ad);
+        System.out.println("Soyad :" + soyad);
+        System.out.println("No :" + ogrNo);
+        System.out.println("Bolum:" + bolum);
+        System.out.println("==========================");
+    }
+}
