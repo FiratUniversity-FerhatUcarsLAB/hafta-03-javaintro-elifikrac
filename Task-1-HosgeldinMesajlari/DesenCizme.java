f474dfa6a592db9a6a74727ebdc537939c6c766c
elifikrac <250541010@firat.edu.tr>
2025-10-22 12:02:27 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/DesenCizme.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class DesenCizme {
+    public static void main(String[] args) {
+        System.out.println("     J A V V A");
+        System.out.println("   J A A V V A A");
+        System.out.println(" J J AAAAA V V AAAAA");
+        System.out.println("   J J A A V A A");
+    }
+}
